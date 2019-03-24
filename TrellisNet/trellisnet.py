e7f5dd169acae69ff6aeb432df8ee083ff8de307
Manideep Kolla <manideep2510@gmail.com>
2019-03-24 19:04:03 +0530

    no injection

--- TrellisNet/trellisnet.py
+++ TrellisNet/trellisnet.py
@@ -56,7 +56,7 @@ class WeightShareConv1d(nn.Module):
             self.dict[(dilation, device, forward)] = F.conv1d(x_1, self.weight1, dilation=dilation)
 
         # Input injection
-        return self.dict[(dilation, device, forward)] + F.conv1d(self.drop(z_1), self.weight2, self.bias2, dilation=dilation)
+        return F.conv1d(self.drop(z_1), self.weight2, self.bias2, dilation=dilation)
 
 
 class TrellisNet(nn.Module):
